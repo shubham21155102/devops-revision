80403bcbe8e1857c9f308949b4d3628faaa624a6
shubham <shubham.kumar.min21@itbhu.ac.in>
2024-11-06 21:50:03 +0530

    added runnable file

--- dbConfig.js
+++ dbConfig.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 console.log('DB_HOST:', process.env.DB_HOST);
 console.log('DB_USER:', process.env.DB_USER);
--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
 docker compose up -d  
+curl -o wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
+chmod +x wait-for-it.sh
